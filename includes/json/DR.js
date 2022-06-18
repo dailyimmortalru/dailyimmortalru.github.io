29b7f4d00dc9223cff602bdf5829f78f2b7dc6a5
dailyimmortalru <107749014+dailyimmortalru@users.noreply.github.com>
2022-06-18 13:46:26 +0500

    Add files via upload

---
+++ includes/json/DR.js
@@ -0,0 +1,128 @@
+var dailies = {
+	"daily-side": {
+        task: "Искатель приключений",
+        url: "#",
+        short: true,
+        desc: "Выполнение побочных заданий от искателя приключений",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "daily-battlegrounds": {
+        task: "Поле битвы",
+        time: "8-12-18-22",
+        duration: 2,
+        url: "#",
+        short: true,
+        desc: "Повторяется 3 раза для получения награды",
+        img: "placeholder",
+        boxcount: 3
+    },
+	"daily-bestiary": {
+        task: "Бестиарий",
+        url: "#",
+        short: true,
+        desc: "Завершить 3 страницы Бестиария",
+        img: "placeholder",
+        boxcount: 3
+    },
+    "daily-bounty": {
+        task: "Поручения",
+        url: "#",
+        short: true,
+        desc: "Выполните 8 поручений (можно стакать до 24, если не все задания выполнены за день)",
+        img: "placeholder",
+        boxcount: 8
+    },
+    "daily-faction": {
+        task: "Ежедневные контракты",
+        url: "#",
+        short: true,
+        desc: "Выполните ежедневный контракт с тенью (можно стакать до 5)",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "daily-challenge-reward": {
+        task: "Вознаграждения за ежедневные испытания",
+        url: "#",
+        short: true,
+        desc: "Получите награды за ежедневные задания в портале дерзаний",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "daily-elder": {
+        task: "Древниц портал",
+        url: "#",
+        short: true,
+        desc: "Соберите 3 награды Благословение Дедессы в Древних порталах",
+        img: "placeholder",
+        boxcount: 3
+    },
+    "daily-first": {
+        task: "Первое убийство дня",
+        url: "#",
+        short: true,
+        desc: "Победите хотя бы одного монстра в день, чтобы получить бесплатную награду.",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "daily-reward": {
+        task: "Бесплатные ежедневные награды",
+        url: "#",
+        short: true,
+        desc: "Возьмите свой сундук с ежедневными вознаграждениями в магазине в разделе комплекты",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "daily-hilt": {
+        task: "Собиратель эфесов",
+        url: "#",
+        short: true,
+        desc: "Покупайте товары у собирателя эфесов",
+        img: "hilts",
+        boxcount: 1
+    },
+    "daily-sanctuary": {
+        task: "Наследие Хорадримов",
+        url: "#",
+        short: true,
+        desc: "Выполните задание Святилище  Ибн Фахда и откройте сундуки",
+        img: "key",
+        boxcount: 1
+    },
+    "daily-crest": {
+        task: "Редкий Глиф",
+        url: "#",
+        short: true,
+        desc: "Соберите бесплатный {rare_crest} из древнего портала",
+        img: "rare_crest",
+        boxcount: 1
+    },
+	"daily-lottery": {
+        task: "Лотерея теней",
+        time: "12-18-21",
+        duration: 1,
+        url: "#",
+        short: true,
+        desc: "Участвуйте в лотерее теней",
+        img: "signet",
+        boxcount: 1
+    },
+	"assembly": {
+        task: "Собрание",
+        time: "18",
+        duration: 2,
+        day: "1-2-3-4-5-6",
+        url: "#",
+        desc: "Соберите все 4 благословения.",
+        img: "assembly",
+        boxcount: 4
+    },
+    "daily-zone": {
+        task: "Событие зоны",
+        url: "#",
+        short: true,
+        desc: "Запуск мировых событий, происходящих в определенные дни, таких как Древний кошмар и Карета с привидениями",
+        img: "placeholder",
+        boxcount: 1
+    },
+};
---
+++ includes/json/MR.js
@@ -0,0 +1,9 @@
+var monthlies = {
+    "hilt": {
+        task: "Ежемесячный собиратель эфесов",
+        url: "#",
+        desc: "Покупайте ежемесячно такие предметы, как{leg_crest}",
+        img: "hilts",
+        boxcount: 1
+    },
+};
